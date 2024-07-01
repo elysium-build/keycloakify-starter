16c1701757fd7b0cea987aeb220ae13b0c80d47c
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-07-01 21:50:35 +0200

    Demonstrate how to call the account API

---
+++ src/account/oidc.ts
@@ -0,0 +1,8 @@
+import { createReactOidc } from "oidc-spa/react";
+
+export const { OidcProvider, useOidc } = createReactOidc({
+    issuerUri: `${window.location.origin}${window.location.pathname.split("/").slice(0, 3).join("/")}`,
+    clientId: "account-console",
+    publicUrl: undefined,
+    isAuthGloballyRequired: true
+});
